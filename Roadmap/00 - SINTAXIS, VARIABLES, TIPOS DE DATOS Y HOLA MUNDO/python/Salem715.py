a03695c258e67b8fd4684bd4656b8f7e5dcd1def
Salem715 <Aprendiz0736@gmail.com>
2024-12-12 13:09:33 -0600

    Create Salem715.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Salem715.py
@@ -0,0 +1,33 @@
+# https://www.python.org/
+
+# Comentario en una linea
+
+"""
+Esto tambien es 
+un comentario 
+en varias lineas
+"""
+
+´´´
+Esto tambien es
+un comentario
+en varias lineas
+´´´
+
+my_vaiable  = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" # por convención
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_string = "Mi cadena de texto"
+my_other_string = "Mi otra cadena de texto"
+
+print("¡Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
